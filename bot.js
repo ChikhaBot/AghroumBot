7103f26449160c67a947a536120373a7c883b4e4
Anas Mazouni <madadj4@gmail.com>
2021-10-27 17:55:16 +0200

    wasafat

--- bot.js
+++ bot.js
@@ -12,7 +12,8 @@ const token = process.env.TOKEN ?? 'TOKEN';
 const fs = require('fs')
 const bankContent = fs.readFileSync('./bank.txt', {encoding: 'utf-8'})
 const responses=  bankContent.split('\n')
-
+const fetch = require('node-fetch');
+const {parse} = require('node-html-parser');
 
 const client = new Client({
 	intents: [
@@ -29,6 +30,33 @@ client.once('ready', () => {
 });
 
 let cnt = {}
+
+// function to get the raw data
+const getRawData = (URL) => {
+   return fetch(URL)
+      .then((response) => response.text())
+      .then((data) => {
+         return data;
+      });
+};
+
+// URL for data
+const domain = "https://mawdoo3.com"
+const URL = domain+"/"+"%D8%AA%D8%B5%D9%86%D9%8A%D9%81:%D8%A3%D9%83%D9%84%D8%A7%D8%AA_%D8%B3%D8%B1%D9%8A%D8%B9%D8%A9";
+
+// start of the program
+const getRecipe = async () => {
+   const RecipeRawData = await getRawData(URL);
+   const loadedData = parse(RecipeRawData);
+   const recipes = loadedData.querySelector("#grid").childNodes.filter(node => node.rawTagName === "li");
+   const randomRecipe = recipes[Math.floor(Math.random() * recipes.length)];
+   if(randomRecipe) {
+    const recipeLink = domain + randomRecipe.querySelector("a").attributes.href
+    return recipeLink;
+   }
+   return null
+};
+
 
 client.on('message', function (message) {
 	const content = message.content
@@ -60,6 +88,15 @@ client.on('message', function (message) {
 				cnt[authorId]++
 				message.reply(`Li7wak ${cnt[authorId]}-0`);
 			}
+      if(content.includes('jou3')){
+        getRecipe().then(recipe => {
+          if(recipe){
+            message.reply(recipe)
+          }else{
+            message.reply("No recipe found, 9wd 3end mok!")
+          }
+        })
+      }
 		break
 	}
 });
