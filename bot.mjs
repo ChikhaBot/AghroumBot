c6e8a0362f6393fd1d22f4ceca338aff3b3c1ff1
Anas Mazouni <madadj4@gmail.com>
2022-01-21 08:18:30 -0800

    Update bot.mjs

--- bot.mjs
+++ bot.mjs
@@ -93,7 +93,7 @@ client.on('message', function (message) {
 			return message.reply('<@214522510639759360> <@447912513653309442> <@342071051183849484> <@532675052035112989> <@689907859919601689> ');
 			break;
 		default:
-			if (Math.random() < proba && authorId != client.user.id && isTargetable && message.channel.id == "912069547987730444")
+			if (Math.random() < proba && authorId != client.user.id && isTargetable && message.channel.id == "929539397253222440")
 			 {
 				// Reply to that random person with a random reply
 				const randomReply = responses[Math.floor(Math.random() * responses.length)]
